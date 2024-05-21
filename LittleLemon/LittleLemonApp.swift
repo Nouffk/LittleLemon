d43ce503b6876dd9a2e16aab5b1688e9d65d664c
Nouf <nalknedri@stu.kau.edu.sa>
2024-05-22 02:58:59 +0300

    Initial Commit

---
+++ LittleLemon/LittleLemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonApp.swift
+//  LittleLemon
+//
+//  Created by Nouf Faisal  on 14/11/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
